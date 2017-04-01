aed66e2413dbf11530d1a05994106939acd849c0
awdsawds211 <awdsawds211@qq.com>
2017-04-01 18:00:11 +0800

    keep
    2

---
+++ files/启动页/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o,p,q,r],s,_(t,u,v,w,x,y,z,_(),A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,E,M,E,N,O,P,null,Q,R,S,T,U,V,W,R),X,_(),Y,_(),Z,_(ba,[])),bb,_(),bc,_());}; 
+var b="url",c="启动页.html",d="generationDate",e=new Date(1491040527727.47),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="hours",l="minutes",m="seconds",n="msec",o="nHours",p="nMinutes",q="nSeconds",r="nMsec",s="page",t="packageId",u="68e53d157fb64dc2b4db566f518e029b",v="type",w="Axure:Page",x="name",y="启动页",z="notes",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="near",F="fill",G="fillType",H="solid",I="color",J=0xFFCCCCCC,K="image",L="imageHorizontalAlignment",M="imageVerticalAlignment",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="Applied Font",W="borderWidth",X="adaptiveStyles",Y="interactionMap",Z="diagram",ba="objects",bb="masters",bc="objectPaths";
+return _creator();
+})());
